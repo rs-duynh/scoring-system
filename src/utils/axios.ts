3e4a4b7cb1a863dbc60f867bdb4354b791eb27fc
duy <duynH@runsystem.net>
2025-02-18 07:53:18 +0700

    asdas

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,15 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000'
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  withCredentials: true
 });
 
 export default api;
